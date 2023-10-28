8590c64b8dd91e5e751927e886f08bf432d6b234
dimitricbrown <dimitricbrown@outlook.com>
2023-10-28 16:24:13 -0500

    Api data calls have been added for Events, Categories, and Users

---
+++ api/eventData.js
@@ -0,0 +1,136 @@
+import { clientCredentials } from '../utils/client';
+
+const dbUrl = clientCredentials.databaseURL;
+
+// GET ALL EVENTS
+const getEvents = () => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/events`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// GET A SINGLE EVENT DETAILS
+const getSingleEvent = (id) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/events/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        data = await res.json();
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+// CREATE AN EVENT
+const createEvent = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/events`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        data = await res.json();
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+// UPDATE AN EVENT
+const updateEvent = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/events/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(resolve)
+    .catch(reject);
+});
+
+// DELETE AN EVENT
+const deleteEvent = (id) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/events/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+// GET ALL USERS FOR AN EVENT
+const getAllUsersOnEvent = (id) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/eventUser/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// ADD USER TO AN EVENT
+const addUsersToEvent = (userId, eventId) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/eventUser/${userId}/${eventId}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// DELETE AN EVENT FROM A USER
+const deleteEventFromUser = (eventId, userId) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/eventUser/${eventId}/${userId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        resolve(data);
+      }
+    })
+    .catch(reject);
+}); 
+
+export {
+  getEvents,
+  getSingleEvent,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+  getAllUsersOnEvent,
+  addUsersToEvent,
+  deleteEventFromUser,
+};
---
+++ api/userData.js
@@ -0,0 +1,119 @@
+import { clientCredentials } from '../utils/client';
+
+const dbUrl = clientCredentials.databaseURL;
+
+// GET ALL USERS
+const getUsers = () => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/users`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// GET A SINGLE USER DETAILS
+const getSingleUser = (id) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/users/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        data = await res.json();
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+// CREATE AN USER
+const createUser = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        data = await res.json();
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+// UPDATE AN USER
+const updateUser = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/users/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(resolve)
+    .catch(reject);
+});
+
+// DELETE AN USER
+const deleteUser = (id) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/users/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+// GET ALL EVENTS FOR AN USER
+const getAllSignedUpEventsOnUser = (uid) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/eventUser/${uid}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+// GET ALL CREATED EVENTS FOR AN USER
+const getAllCreatedEventsOnUser = (createdBy) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/eventUser/${createdBy}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+export {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  getAllSignedUpEventsOnUser,
+  getAllCreatedEventsOnUser,
+};
